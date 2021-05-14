5c7b9ab35a0d0c717cb644a6af79d498ba5bf5b5
William Crownover <williamcrownover1@gmail.com>
2021-05-14 13:08:31 -0700

    Adding comments to explain parts of code and function.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //DECLARE GLOBAL DATA
-//Character Sets
+//Character Sets as individaul strings
 var specialCharacters = "@%+'!#$^?:,(){}[]~-_.";
 var numericCharacters = "0123456789";
 var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
@@ -21,6 +21,7 @@ var characterSetNames = [
   "uppercase"
 ]
 
+
 //----------------------------------------------------------------------------
 // Assignment Code 
 var generateBtn = document.querySelector("#generate");
@@ -38,30 +39,46 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 //----------------------------------------------------------------------------
 
+
 //Creates a character string to be sent back to writePassword(), var password
 function generatePassword() {
+  //The length of the password to create
   var passwordLength = askPasswordLength();
+  //The users choices as to which character sets to use are stored here
   var userSetChoices = [];
+  //One string holding all requested characters, starts with nothing
   var combinedCharacters = "";
+  //The randomized password string to return once guaranteed
   var passwordRandomized = "";
 
+
+  //If askPasswordLength() returned "tryAgain" the function stops and resets the process
   if (passwordLength === "tryAgain") {
+    //A blank string is sent back to the textbox
     return "";
   }
 
+  //Asks the user to choose what character sets they want to use
   for (var i = 0; i < characterSetNames.length; i++ ) {
+    //Each of the four choices is stored in the array by retrieving a true or false using a confirm box
     userSetChoices[i] = confirm("Click OK to confirm including "+characterSetNames[i]+" characters.");
 
+    //Each time a user clicks "OK" the corresponding character set is added to `combinedCharacters`
     if (userSetChoices[i]) {
       combinedCharacters += allCharacterSets[i];
     }
-
+    //On the fourth loop the combined string is checked to make sure at least one set was choosen
     if (i == characterSetNames.length-1 && combinedCharacters === "") {
+      //If nothing was selected an alert informs the user to pick one character set
       alert("Please include at least one set of characters to use.");
+      //A blank string is sent back to the textbox
+      return "";
     }
   }
 
+  //A randomized password is generated adding characters to the string for the length requested
   for (var i = 0; i < passwordLength; i++ ) {
+    //Each random character is added to the end of the string
     passwordRandomized += combinedCharacters[Math.floor(Math.random()*combinedCharacters.length)]
   }
   
@@ -74,20 +91,24 @@ function generatePassword() {
     passwordRandomized = guaranteePasswordContainsSets(passwordLength, sumOfSetChoices, userSetChoices, passwordRandomized);
     return passwordRandomized;
   }
-
-}
+} //----------------------------------------------------------------------------
 
 //Collects a value from the user for their password length between 8 and 128
 function askPasswordLength() {
+  //Using prompt immediately takes the user input value and assigns it to `passwordLength`
   passwordLength = prompt("How many characters would you like your password to contain?", "Please enter a value between 8 and 128");
   
+  //We need to make sure the length fits the criteria of between 8 and 128
   if (passwordLength < 8 || passwordLength > 128) {
+    //Alert informs the user that the input was invalid
     alert("Password length must be between 8 and 128 characters.");
+    //This string is returned instead of a number which will trigger generatePassword() to stop running
     return "tryAgain";
   } else {
+    //Otherwise the length is valid and returned to be stored as the `passwordLength`
     return passwordLength;
   }
-}
+} //----------------------------------------------------------------------------
 
 //Divide randomized password into segments and inserts a character from user selected sets in rare case they were not automatically included
 function guaranteePasswordContainsSets(passwordLength, sumOfSetChoices, userSetChoices, passwordRandomized) {
